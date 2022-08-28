9d67f13f16be6a4e0f935049cb63920b37e1ef83
dima-yarakhovich <dima020490@tut.by>
2022-08-28 22:36:03 +0300

    hw2.3

--- src/Main.java
+++ src/Main.java
@@ -1,37 +1,62 @@
 public class Main {
     public static void main(String[] args) {
 
-        var dog= 8 ;
-        System.out.println( dog );
-        var cat =  3.6;
-        System.out.println( cat );
-        var paper =  763789 ;
+        var dog = 8;
+        System.out.println(dog);
+        var cat = 3.6;
+        System.out.println(cat);
+        var paper = 763789;
         System.out.println(paper);
         dog = dog + 4;
-        System.out.println( dog );
+        System.out.println(dog);
         cat = cat + 4;
-        System.out.println( cat );
+        System.out.println(cat);
         paper = paper + 4;
         System.out.println(paper);
         dog = (int) (dog - 3.5);
-        System.out.println( dog );
+        System.out.println(dog);
         cat = cat - 1.6;
-        System.out.println( cat );
+        System.out.println(cat);
         paper = paper - 7639;
         System.out.println(paper);
         var friend = 19;
-        System.out.println( friend );
+        System.out.println(friend);
         friend = friend + 2;
-        System.out.println( friend );
+        System.out.println(friend);
         friend = friend / 7;
-        System.out.println( friend );
+        System.out.println(friend);
         var frog = 3.5;
-        System.out.println( frog );
+        System.out.println(frog);
         frog = frog * 10;
-        System.out.println( frog );
+        System.out.println(frog);
         frog = frog / 3.5;
-        System.out.println( frog );
+        System.out.println(frog);
         frog = frog + 4;
-        System.out.println( frog );
+        System.out.println(frog);
+
+        var boxerOne = 78.2;
+        var boxerTwo = 82.7;
+        var boxerWeight = boxerOne + boxerTwo;
+        System.out.println("Общий вес двух бойцов равен " + boxerWeight + " кг");
+        var differenceWeight = boxerOne - boxerTwo;
+        System.out.println("Разница веса между двумя бойцами составляет " + differenceWeight + " кг ");
+        var differenceWeight1 = boxerTwo - boxerOne;
+        System.out.println("Разница веса между двумя бойцами составляет " + differenceWeight1 + " кг ");
+        var differenceWeight2 =boxerTwo % boxerOne;
+        System.out.println("Разница веса между двумя бойцами составляет " + differenceWeight2 + " кг ");
+
+        var timeTotal = 640;
+        var timeOne = 8;
+        var quantityWorker = timeTotal / timeOne;
+        System.out.println("Всего работников в компании работает "+ quantityWorker + " человек");
+
+        quantityWorker = quantityWorker + 94;
+        var timeTotal1 = timeTotal / quantityWorker;
+        System.out.println("Если в компании работает " + quantityWorker + " человека, то всего " + timeTotal1 + " часа работы может быть поделено между сотрудниками ");
+
+
+
     }
+
+
 }
